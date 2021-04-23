45f597adcf2f32220797cafb774c6b00649afbd0
jaasonw <jayywong92@gmail.com>
2021-04-22 18:53:43 -0700

    add react

---
+++ client/src/App.tsx
@@ -0,0 +1,34 @@
+import logo from './logo.svg';
+import './App.css';
+import { useEffect, useState } from 'react';
+
+function App() {
+
+  const [text, setText] = useState("");
+
+  useEffect(() =>  {
+    fetch("/api/delicate").then(response => {
+      response.text().then((data) => {
+        setText(data)
+      })
+    });
+  });
+
+  console.log(text)
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <div>
+          {text}
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
--- index.js
+++ index.js
@@ -4,11 +4,16 @@ const app = express()
 var path = require("path");
 
 app.use('/static', express.static(path.join(__dirname, 'static')))
+app.use(express.static(path.join(__dirname, 'client/build')))
 
-app.get('/', (req, res) => {
-    res.sendFile("./static/delicate.txt", { root: __dirname });
+app.get('/', async (req, res) => {
+  res.sendFile(path.join(__dirname + '/client/build/index.html'));
+})
+
+app.get('/api/delicate', async (req, res) => {
+  res.sendFile("./static/delicate.txt", { root: __dirname });
 })
 const PORT = process.env.PORT || 5000
 app.listen(PORT, () => {
-    console.log(`Hosting server on port: ${PORT}`);
+  console.log(`Hosting server on port: ${PORT}`);
 })
