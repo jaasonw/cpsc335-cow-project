14f71219e5c2f710b6a08a7ad37367048cfbc167
jaasonw <jayywong92@gmail.com>
2021-04-25 12:13:49 -0700

    connect to the database

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,5 +1,27 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
+import dotenv from 'dotenv';
+import { Client } from 'pg';
 
-export default (req: NextApiRequest, res: NextApiResponse) => {
-  res.status(200).json({ name: 'John Doe' })
+dotenv.config();
+
+const client = new Client({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
+
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+  client.connect();
+  try {
+    let query = await client.query("select * from users where id like 'asdf%';")
+    console.log(query.rows)
+    if (query.rows.length > 0)
+      return res.status(200).json(query.rows)
+    else
+      return res.status(404).end()
+  } catch (error) {
+    console.log(error)
+  }
+  client.end()
 }
