71c704170828827a450121db72439bba15385385
jaasonw <jayywong92@gmail.com>
2021-05-10 11:18:37 -0700

    date added

--- pages/api/addCow.ts
+++ pages/api/addCow.ts
@@ -3,13 +3,14 @@ import { dbQuery } from "../../components/dbQuery";
 import { Cow } from "../../components/definitions/Cow";
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
+  const date = new Date().toISOString().slice(0, 19).replace("T", " ");
   if (req.method === "POST") {
     const cow = req.body as Cow;
     try {
-      await dbQuery("insert into cows(herd_id, owner_id) values($1, $2)", [
-        cow.herd_id,
-        cow.owner_id,
-      ]);
+      await dbQuery(
+        "insert into cows(herd_id, owner_id, date_acquired) values($1, $2)",
+        [cow.herd_id, cow.owner_id, date]
+      );
     } catch (error) {
       console.log(error);
     }
